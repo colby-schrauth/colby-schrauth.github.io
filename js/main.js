b3df174891ccb82b879b8d649f08d76661ecdfe9
Andrew Colby Schrauth <cschrauth@Andrews-MacBook-Pro.local>
2020-03-22 21:45:05 -0700

    add a javascript folder along with charting and table examples

---
+++ js/main.js
@@ -0,0 +1,70 @@
+const CHART = document.getElementById("linechart");
+console.log(CHART);
+
+let myChart = new Chart(CHART, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+var tableData = [
+    {id:1, name:"Billy Bob", age:"12", gender:"male", height:1, col:"red", dob:"", cheese:1},
+    {id:2, name:"Mary May", age:"1", gender:"female", height:2, col:"blue", dob:"14/05/1982", cheese:true},
+]
+
+var table = new Tabulator("#example-table", {
+	data:tableData,           //load row data from array
+	layout:"fitColumns",      //fit columns to width of table
+	responsiveLayout:"hide",  //hide columns that dont fit on the table
+	tooltips:true,            //show tool tips on cells
+	addRowPos:"top",          //when adding a new row, add it to the top of the table
+	history:true,             //allow undo and redo actions on the table
+	pagination:"local",       //paginate the data
+	paginationSize:7,         //allow 7 rows per page of data
+	movableColumns:true,      //allow column order to be changed
+	resizableRows:true,       //allow row order to be changed
+	initialSort:[             //set the initial sort order of the data
+		{column:"name", dir:"asc"},
+	],
+	columns:[                 //define the table columns
+		{title:"Name", field:"name", editor:"input"},
+		{title:"Task Progress", field:"progress", align:"left", formatter:"progress", editor:true},
+		{title:"Gender", field:"gender", width:95, editor:"select", editorParams:{values:["male", "female"]}},
+		{title:"Rating", field:"rating", formatter:"star", align:"center", width:100, editor:true},
+		{title:"Color", field:"col", width:130, editor:"input"},
+		{title:"Date Of Birth", field:"dob", width:130, sorter:"date", align:"center"},
+		{title:"Driver", field:"car", width:90,  align:"center", formatter:"tickCross", sorter:"boolean", editor:true},
+	],
+});
